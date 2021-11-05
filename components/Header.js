e9875a290f9c15dc63f88d09e53aed53081b0e7d
Felix <felixrunye@gmail.com>
2021-11-05 16:39:15 +0300

    updated header with image

--- components/Header.js
+++ components/Header.js
@@ -7,14 +7,14 @@ function Header() {
        <div className="">
             <div className="flex justify-between max-w-6xl items-center mx-5 xl:mx-auto ">
                 {/* left */}
-                <div className="relative hidden lg:inline-grid h-24 w-24 cursor-pointer">
+                <div className="relative  h-24 w-24 cursor-pointer">
                     <Image  src="https://links.papareact.com/ocw" layout="fill" className="" objectFit="contain"/>
                 </div>
-                <div className="relative lg:hidden h-10 w-10 flex-shrink-0 cursor-pointer">
+                {/* <div className="relative lg:hidden h-10 w-10 flex-shrink-0 cursor-pointer">
                     <Image  src="https://links.papareact.com/jjm" layout="fill" className="" objectFit="contain"/>
-                </div>
+                </div> */}
                 {/* middle */}
-                <div className="max-w-xs">
+                <div className="max-w-xs hidden md:inline-flex">
                     <div className="flex items-center rounded-md space-x-1 p-3 mt-1  relative">
                         <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
                             <SearchIcon className="h-5 text-gray-500"/>
@@ -25,8 +25,9 @@ function Header() {
                 </div>
                 {/* right */}
                 <div className="flex space-x-1">
-                    <HomeIcon className="h-10" />
-                    <PaperAirplaneIcon className="h-10" />
+                    <HomeIcon className="h-7 text-gray-500" />
+                    <PaperAirplaneIcon className="h-7 text-gray-500" />
+                    <PlusCircleIcon className="h-7 text-gray-500" />
                 </div>
             </div>
        </div>
