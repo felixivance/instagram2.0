2a396794ac13af4dcbb46f4db870b45c849e881a
Felix <felixrunye@gmail.com>
2021-11-05 15:26:52 +0300

    header design with logo

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,20 @@
+import Image from 'next/image';
+
 function Header() {
     return (
-        <div>
-            
-        </div>
+       <div className="">
+            <div className="flex justify-between max-w-6xl">
+                {/* left */}
+                <div className="relative hidden lg:inline-grid h-24 w-24 cursor-pointer">
+                    <Image  src="https://links.papareact.com/ocw" layout="fill" className="" objectFit="contain"/>
+                </div>
+                <div className="relative lg:hidden h-10 w-10 flex-shrink-0 cursor-pointer">
+                    <Image  src="https://links.papareact.com/jjm" layout="fill" className="" objectFit="contain"/>
+                </div>
+                {/* middle */}
+                {/* right */}
+            </div>
+       </div>
     )
 }
 
