bab21fe6f4171207dc48edf16a271ce6cc2d2766
Felix <felixrunye@gmail.com>
2021-11-05 15:51:49 +0300

    focusing on border with relative search icon

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import { SearchIcon, PlusCircleIcon, UserGroupIcon, HeartIcon, PaperAirplaneIcon, MenuIcon } from '@heroicons/react/outline';
 
 function Header() {
     return (
@@ -12,6 +13,13 @@ function Header() {
                     <Image  src="https://links.papareact.com/jjm" layout="fill" className="" objectFit="contain"/>
                 </div>
                 {/* middle */}
+                <div className="flex items-center rounded-md space-x-1 p-3 mt-1  relative">
+                    <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+                        <SearchIcon className="h-5 text-gray-500"/>
+                    </div>
+                    <input type="text" placeholder="search" className="bg-gray-50 block w-full pl-10 focus:border-black focus:ring-black sm:text-sm
+                    rounded-md border-gray-300" />
+                </div>
                 {/* right */}
             </div>
        </div>
