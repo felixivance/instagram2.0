455de92254244d70d8709224b3705cdd7cba8046
Felix <felixrunye@gmail.com>
2021-11-07 02:39:20 +0300

    feed building

--- components/Stories.js
+++ components/Stories.js
@@ -25,7 +25,7 @@ function Stories() {
     }, [])
 
     return (
-        <div className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 border rounded-sm 
+        <div className="flex space-x-1 p-3 bg-white mt-8 border-gray-200 border rounded-sm 
         overflow-x-scroll scrollbar-thin scrollbar-thumb-black ">
            {
                suggestions.map((profile,index) => (
