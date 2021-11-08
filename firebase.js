8a4eb3c1fe90d5dd3683248b949c3aa1281023b5
Felix <felixrunye@gmail.com>
2021-11-08 20:42:59 +0300

    added firebase

---
+++ firebase.js
@@ -0,0 +1,14 @@
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCKOKbFXnA1tPSivh8hzZZdKHKLc19tuT4",
+    authDomain: "friendlychat-21baa.firebaseapp.com",
+    databaseURL: "https://friendlychat-21baa.firebaseio.com",
+    projectId: "friendlychat-21baa",
+    storageBucket: "friendlychat-21baa.appspot.com",
+    messagingSenderId: "360653608130",
+    appId: "1:360653608130:web:fbde06937017329229d75b"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
