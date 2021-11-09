ffdc71a6f7a43ba533fadf60022567818f19d8a3
Felix <felixrunye@gmail.com>
2021-11-09 08:48:51 +0300

    added custom sign in page

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,9 +1,30 @@
-function signin() {
+import { getProviders, signIn } from "next-auth/react";
+
+function signin({providers}) {
     return (
-        <div>
-            <p>i am sign in</p>
-        </div>
+       <>
+            {
+                Object.values(providers).map((provider, index)=>(
+                    <div key={index} className="">
+                        <button onClick={()=> signIn(provider.id) }> 
+                            Sign in with {provider.name}
+                         </button>
+                    </div>
+                ))
+            }
+       </>
     )
 }
 
 export default signin
+
+
+export async function getServerSideProps(){
+    const providers = await getProviders();
+
+    return {
+        props:{
+            providers
+        }
+    }
+}
