56b4bbbb2f9419566f295da1ef1c6cd30236d06e
Felix <felixrunye@gmail.com>
2021-11-09 10:20:11 +0300

    improved sign in to navigate user to homepage

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -5,14 +5,15 @@ function signin({providers}) {
     return (
        <>
             <Header />
-            <div className="flex flex-col items-center pt-10">
+            <div className="flex flex-col items-center justify-center min-h-screen -mt-56 px-14">
                 <img className="w-80" src="https://links.papareact.com/ocw" alt="" />
 
-                <div className="mt-40">
+                <div className="mt-20">
                 {
                     Object.values(providers).map((provider, index)=>(
                         <div key={index} className="">
-                            <button className="p-3 bg-blue-500 text-white rounded-md" onClick={()=> signIn(provider.id) }> 
+                            <button className="p-3 bg-blue-500 text-white rounded-md" 
+                            onClick={()=> signIn(provider.id, { callbackUrl:'/'}) }> 
                                 Sign in with {provider.name}
                             </button>
                         </div>
