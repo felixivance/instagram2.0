35ccd89c4b35907e470bfccc8105549e65de4506
Felix <felixrunye@gmail.com>
2021-11-10 15:16:30 +0300

    custom modal

--- components/Modal.js
+++ components/Modal.js
@@ -5,45 +5,108 @@ import { Fragment } from "react";
 
 function Modal() {
     const [open, setOpen]  = useRecoilState(modalState);
-
+    console.log(open);
     return (
-        <Transition.Root className="" show={open} as={Fragment}>
+        // <!-- This example requires Tailwind CSS v2.0+ -->
+<div  className={`${open ? '': 'hidden'} fixed z-10 inset-0 overflow-y-auto `} aria-labelledby="modal-title" role="dialog" aria-modal="true">
+  <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+    {/* <!--
+      Background overlay, show/hide based on modal state.
+
+      Entering: "ease-out duration-300"
+        From: "opacity-0"
+        To: "opacity-100"
+      Leaving: "ease-in duration-200"
+        From: "opacity-100"
+        To: "opacity-0"
+    --> */}
+    <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" aria-hidden="true"></div>
+
+    {/* <!-- This element is to trick the browser into centering the modal contents. --> */}
+    <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+
+    {/* <!--
+      Modal panel, show/hide based on modal state.
+
+      Entering: "ease-out duration-300"
+        From: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+        To: "opacity-100 translate-y-0 sm:scale-100"
+      Leaving: "ease-in duration-200"
+        From: "opacity-100 translate-y-0 sm:scale-100"
+        To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+    --> */}
+    <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+      <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+        <div className="sm:flex sm:items-start">
+          <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+            {/* <!-- Heroicon name: outline/exclamation --> */}
+            <svg className="h-6 w-6 text-red-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+            </svg>
+          </div>
+          <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+            <h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-title">
+              Deactivate account
+            </h3>
+            <div className="mt-2">
+              <p className="text-sm text-gray-500">
+                Are you sure you want to deactivate your account? All of your data will be permanently removed. This action cannot be undone.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+        <button type="button" className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">
+          Deactivate
+        </button>
+        <button type="button" className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+        onClick={()=>setOpen(false)}>
+          Cancel
+        </button>
+      </div>
+    </div>
+  </div>
+</div>
+        // <Transition.Root classNameName="" show={open} as={Fragment}>
             
-            <Dialog open={open} onClose={() => setOpen(false)} as="div" 
-            className="fixed z-10 inset-0 overflow-y-auto" onClose={()=>setOpen(false)}>
-                <div className="flex items-end justify-center min-h-[800px] sm:min-h-screen 
-                pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-                    <Transition.Child className="" 
-                    as={Fragment} enter="ease-out duration-300" enterFrom="opacity-0"
-                    enterTo="opacity-100" leave="ease-in duration-200" leaveFrom="opacity-100"
-                    leaveTo="opacity-0">
+        //     <Dialog open={open} onClose={() => setOpen(false)} as="div" 
+        //     classNameName="fixed z-10 inset-0 overflow-y-auto" onClose={()=>setOpen(false)}>
+        //         <div classNameName="flex items-end justify-center min-h-[800px] sm:min-h-screen 
+        //         pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+        //             <Transition.Child classNameName="" 
+        //             as={Fragment} enter="ease-out duration-300" enterFrom="opacity-0"
+        //             enterTo="opacity-100" 
+        //             leave="ease-in duration-200" 
+        //             leaveFrom="opacity-100"
+        //             leaveTo="opacity-0">
                         
-                        <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"/>
+        //                 <Dialog.Overlay classNameName="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"/>
 
-                            {/* trick browser to center modal items */}
-                        <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
-                             &#8203;
-                        </span>  
-                    </Transition.Child>
+        //                     {/* trick browser to center modal items */}
+                        
+        //             </Transition.Child>
+        //             <span classNameName="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
+        //                      &#8203;
+        //                 </span>  
+        //             <Transition.Child as={Fragment} 
+        //                 enter="ease-out duration-300" 
+        //                 enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+        //                 enterTo="opacity-100 translate-y-0 sm:scale-100" 
+        //                 leave="ease-in duration-200" 
+        //                 leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+        //                 leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
 
-                    <Transition.Child as={Fragment} 
-                        enter="ease-out duration-300" 
-                        enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                        enterTo="opacity-100 translate-y-0 sm:scale-100" 
-                        leave="ease-in duration-200" 
-                        leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-                        leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+        //                     <div classNameName="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left 
+        //                     overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle
+        //                     sm:max-w-sm sm:w-full sm:p-6">
+        //                         <p>Hello</p>
+        //                     </div>
 
-                            <div className="inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left 
-                            overflow-hidden shadow-xl  transform transition-all sm:my-8 sm:align-middle
-                            sm:max-w-sm sm:-full sm:p-6">
-                                <p>Hello</p>
-                            </div>
-
-                    </Transition.Child>
-                </div>
-            </Dialog>
-        </Transition.Root>
+        //             </Transition.Child>
+        //         </div>
+        //     </Dialog>
+        // </Transition.Root>
     )
 }
 
