c2f146c899d69967b58684a06c310063bd1dfb35
Felix <felixrunye@gmail.com>
2021-12-09 21:29:24 +0300

    index reformatting

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,20 @@
-import Head from 'next/head'
-import Feed from '../components/Feed'
-import Header from '../components/Header'
-import Modal from '../components/Modal'
+import Head from "next/head";
+import Feed from "../components/Feed";
+import Header from "../components/Header";
+import Modal from "../components/Modal";
 
 export default function Home() {
   return (
-   <div className="bg-gray-50 h-screen overflow-y-scroll scrollbar-hide">
-     <Head>
-       <title>Instagram 2.0</title>
-     </Head>
-     {/* header */}
-     <Header />
-     {/* feed */}
-     <Feed />
-     {/* modal */}
+    <div className="bg-gray-50 h-screen overflow-y-scroll scrollbar-hide">
+      <Head>
+        <title>Instagram 2.0</title>
+      </Head>
+      {/* header */}
+      <Header />
+      {/* feed */}
+      <Feed />
+      {/* modal */}
       <Modal />
-   </div>  
-  )
+    </div>
+  );
 }
